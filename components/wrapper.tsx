69e9d0966af5a20cf1baed23875aa6118234a738
bmxaddo <adam.dominik@persgroep.net>
2021-01-21 12:44:07 +0100

    first widget is working on front end

--- components/wrapper.tsx
+++ components/wrapper.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
-import ComponentsManager from '@berlingske-media/stock-data.widgets';
+console.log('przed importem');
+// import ComponentsManager from '@berlingske-media/stock-data.widgets/lib/bundle';
+
+console.log('po imporcie');
+
+import {typeOf} from "uri-js/dist/esnext/util";
 
 // import { buildUrl } from '../../utils';
 // import { InstrumentModel } from '../../model';
 // import * as styles from './StockSlider.scss?module';
+function SafeHydrate({ children }) {
+    return (
+        <div suppressHydrationWarning>
+            {typeof window === 'undefined' ? null : children}
+        </div>
+    )
+}
 
 interface Link {
     name: string;
@@ -23,24 +35,30 @@ export const StockSliderWrapper: React.FC<StockSliderWrapperProps> = ({ listId =
         if (!ref.current) {
             return;
         }
+        console.log('typeof window === \'undefined\'', typeof window === 'undefined');
+        if(typeof window === 'undefined') return;
+        import('@berlingske-media/stock-data.widgets/lib/bundle').then(module => {
+            // console.log(ComponentManager.default);
+            console.log(window)
+            window.ComponentsManager.createIndexStockSliderWithProvider(ref.current, {
+                listId: `${listId}`,
+                getHref(instrument: unknown): string {
+                    if (staticLink) {
+                        // @ts-ignore
+                        const isIndex = (instrument).name.indexOf(staticLink.name as string) !== -1;
 
-        ComponentsManager.createIndexStockSliderWithProvider(ref.current, {
-            listId: `${listId}`,
-            getHref(instrument: unknown): string {
-                if (staticLink) {
-                    // @ts-ignore
-                    const isIndex = (instrument).name.indexOf(staticLink.name as string) !== -1;
-
-                    if (isIndex) {
-                        return '/aktier';
+                        if (isIndex) {
+                            return '/aktier';
+                        }
                     }
-                }
 
-                return buildUrl();
-            },
-            staticLink
-        });
+                    return buildUrl();
+                },
+                staticLink
+            });
+        })
+
     }, []);
 
-    return <div ref={ref} className={''} />;
+    return <SafeHydrate><div ref={ref} className={''} /></SafeHydrate>;
 };
