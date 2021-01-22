0b89f447a0e063908ca1c3d08a24b2a87f9b6bbf
bmxaddo <adam.dominik@persgroep.net>
2021-01-22 10:43:36 +0100

    setting up fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
 import '../styles/globals.css'
 
 
-function SafeHydrate({ children }) {
-  return (
-      <div suppressHydrationWarning>
-        {typeof window === 'undefined' ? null : children}
-      </div>
-  )
-}
+// function SafeHydrate({ children }) {
+//   return (
+//       <div suppressHydrationWarning>
+//         {typeof window === 'undefined' ? null : children}
+//       </div>
+//   )
+// }
 
 function MyApp({ Component, pageProps }) {
-  return <SafeHydrate><Component {...pageProps} /></SafeHydrate>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
